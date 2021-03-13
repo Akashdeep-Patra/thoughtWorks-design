20af7adfde378948028b741b63f348e1e2e2da9c
Akashdeep-Patra <adeep8961@gmail.com>
2021-03-13 16:34:25 +0530

    Final iteration

--- cricket.py
+++ cricket.py
@@ -3,55 +3,106 @@ from random import randint
 
 
 class NumberGenerator(ABC):
-    def generate_number(self):
+    def get_score(self):
         pass
 
 
 class RandomNumberGenerator(NumberGenerator):
-    def generate_number(self):
+    def get_score(self):
         return randint(0, 10)
+
+
+class Hitter(NumberGenerator):
+    def get_score(self):
+        array = [0, 4, 6]
+        return array[randint(0, 2)]
+
+
+class Defence(NumberGenerator):
+    def get_score(self):
+        array = [0, 1, 2, 3]
+        return array[randint(0, 3)]
 
 
 class Player:
     def __init__(self, name, number_generator):
         self.name = name
         self.number_generator = number_generator
+        self.scores = []
+        self.total_score = 0
 
-    def generate_number(self):
-        return self.number_generator.generate_number()
+    def get_score(self):
+        score = self.number_generator.get_score()
+        self.total_score += score
+        self.scores.append(score)
+        return score
+
+    def get_total_score(self):
+        return self.total_score
+
+    def get_scores(self):
+        return self.scores
 
 
 class Playable(ABC):
     def play(self):
         pass
 
+    def has_won(self):
+        pass
+
+
+class ShotCompare(ABC):
+    def compare(self, a, b):
+        pass
+
+
+class NormalShotCompare(ShotCompare):
+    def compare(self, a, b):
+        return a == b
+
+
+class LazyShotCompare(ShotCompare):
+    def compare(self, a, b):
+        return False
+
 
 class CricketGame(Playable):
-    def __init__(self, first_player, second_player, overs, target):
-        self.first_player = first_player
-        self.second_player = second_player
+    def __init__(self, first_player, second_player, overs, target, shot_compare):
+        self.batsman = first_player
+        self.bowler = second_player
         self.overs = overs
-        self.first_player_score = 0
+        self.batsman_score = 0
         self.target = target
+        self.iterations = 0
+        self.shot_compare = shot_compare
 
     def play(self):
         for i in range(self.overs * 6):
-            first = self.first_player.generate_number()
-            second = self.second_player.generate_number()
-            print(f'{self.first_player.name}: {first}')
-            print(f'{self.second_player.name}: {second}')
-            if first == second:
-                print(f'{self.first_player.name} has Lost the game')
+            first = self.batsman.get_score()
+            second = self.bowler.get_score()
+            print(f'{self.batsman.name}: {first}')
+            print(f'{self.bowler.name}: {second}')
+            self.iterations += 1
+            # print(type())
+            # print(Defence)
+            # print
+            if self.shot_compare.compare(first, second):
+                print(f'{self.batsman.name} has Lost the game')
                 return
-            self.first_player_score += first
-            if self.first_player_score >= self.target:
-                print(f"{self.first_player.name} has won")
+            self.batsman_score += first
+            if self.has_won():
+                print(f"{self.batsman.name} has won")
                 return
-        print(f"{self.first_player.name} has lost")
+        print(f"{self.batsman.name} has lost")
+
+    def has_won(self):
+        return self.batsman.total_score >= self.target if self.batsman.scores[-1] != self.bowler.scores[-1] \
+            else self.batsman.total_score - self.batsman.scores[-1]
 
 
 if __name__ == '__main__':
-    bats_man = Player("Bats man", RandomNumberGenerator())
+    bats_man = Player("Defence", Defence())
     bowler = Player("Bowler", RandomNumberGenerator())
-    game = CricketGame(bats_man, bowler, 2, 30)
+    game = CricketGame(bats_man, bowler, 2, 10, LazyShotCompare())
     game.play()
---
+++ player_test.py
@@ -0,0 +1,37 @@
+import unittest
+
+from cricket import Player, RandomNumberGenerator, Hitter, CricketGame, Defence
+
+
+class CricketTestClass(unittest.TestCase):
+    def test_game_play_logic(self):
+        bats_man = Player("Batsman", RandomNumberGenerator())
+        bowler = Player("Bowler", Hitter())
+        game = CricketGame(bats_man, bowler, 2, 10)
+        game.play()
+        self.assertTrue(game.has_won(), bats_man.get_total_score() >= 10)
+
+    def test_total_score_of_player(self):
+        batsman = Player("Batsman", RandomNumberGenerator())
+        expected = 0
+        for i in range(6):
+            number = batsman.get_score()
+            expected += number
+        self.assertEqual(expected, batsman.get_total_score())
+
+    def test_normal_batsman_hit(self):
+        batsman = Player("Batsman", RandomNumberGenerator())
+        self.assertTrue(batsman.get_score() in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    def test_hitter_hits_accordingly(self):
+        hitter = Player("Hitter", Hitter())
+        score = hitter.get_score()
+        self.assertTrue(score in [0, 4, 6] and score not in [1, 2, 3, 5, 7, 8, 9, 10])
+
+    def test_defence_hits_accordingly(self):
+        defence = Player("Defence", Defence())
+        score = defence.get_score()
+        self.assertTrue(score in [0, 1, 2, 3] and score not in [4, 5, 6, 7, 8, 9, 10])
+
+    if __name__ == '__main__':
+        unittest.main()
